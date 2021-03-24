4e9e08cb329c57c63d009d6470b01e8930cf199b
DerekHsiehDev <derekhsiehdev@gmail.com>
2021-03-24 16:28:41 -0700

    Create basic login home view and implement ocean waves animation

---
+++ AllMate/Views/Animations/WaveAnimation.swift
@@ -0,0 +1,64 @@
+//
+//  WaveAnimation.swift
+//  AllMate
+//
+//  Created by Derek Hsieh on 3/24/21.
+//
+
+import SwiftUI
+
+struct WaveAnimation: View {
+    let universalSize = UIScreen.main.bounds
+    
+    @State var isAnimated = false
+    var body: some View {
+        
+        ZStack {
+            getSinWave(interval: universalSize.width, amplitude: 200, baseline: -50 + universalSize.height/2)
+                .foregroundColor(Color.init(red: 0.3, green: 0.6, blue: 1).opacity(0.4))
+                .offset(x: isAnimated ? -1*universalSize.width : 0)
+            .animation(
+                Animation.linear(duration: 2)
+                .repeatForever(autoreverses: false)
+            )
+            getSinWave(interval: universalSize.width*1.2, amplitude: 150, baseline: 50 + universalSize.height/2)
+                .foregroundColor(Color.init(red: 0.3, green: 0.6, blue: 1).opacity(0.4))
+                .offset(x: isAnimated ? -1*(universalSize.width*1.2) : 0)
+            .animation(
+                Animation.linear(duration: 5)
+                .repeatForever(autoreverses: false)
+            )
+        }.onAppear(){
+            self.isAnimated = true
+        }
+            
+    }
+    
+    func getSinWave(interval:CGFloat, amplitude: CGFloat = 100 ,baseline:CGFloat = UIScreen.main.bounds.height/2) -> Path {
+        Path{path in
+            path.move(to: CGPoint(x: 0, y: baseline
+            ))
+            path.addCurve(
+                to: CGPoint(x: 1*interval,y: baseline),
+                control1: CGPoint(x: interval * (0.35),y: amplitude + baseline),
+                control2: CGPoint(x: interval * (0.65),y: -amplitude + baseline)
+            )
+            path.addCurve(
+                to: CGPoint(x: 2*interval,y: baseline),
+                control1: CGPoint(x: interval * (1.35),y: amplitude + baseline),
+                control2: CGPoint(x: interval * (1.65),y: -amplitude + baseline)
+            )
+            path.addLine(to: CGPoint(x: 2*interval, y: universalSize.height))
+            path.addLine(to: CGPoint(x: 0, y: universalSize.height))
+            
+            
+        }
+    
+    }
+}
+
+struct WaveAnimation_Previews: PreviewProvider {
+    static var previews: some View {
+        WaveAnimation()
+    }
+}
--- AllMate/Views/Login /LoginHomeView.swift
+++ AllMate/Views/Login /LoginHomeView.swift
@@ -8,15 +8,121 @@
 import SwiftUI
 
 struct LoginHomeView: View {
+    
+    @State var isAnimated = false
+    let universalSize = UIScreen.main.bounds
+    
     var body: some View {
+       
         
-        VStack {
-                Text("AllMate")
-                    .foregroundColor(.white)
+        NavigationView {
+            ZStack {
+                
+                    Color.black.edgesIgnoringSafeArea(.all)
+                
+              
+                
+                WaveAnimation()
+                
+                
+   
+                
+                VStack {
+                    HStack {
+                        Text("AllMate")
+                            .foregroundColor(.white)
+                            .font(Font.custom("Pacifico-Regular", size: 50))
+                            .padding(.leading, 30)
+                        
+                        Spacer()
+                    }
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .leading) {
+                        Text("Social Media for Students")
+                            .foregroundColor(.white)
+                            .font(.system(size: 30, weight: .bold, design: .default))
+                        
+                        Text("Collaborate with students in the same classes")
+                            .foregroundColor(Color.white.opacity(0.7))
+                            .font(.system(size: 23, weight: .medium, design: .default))
+                    }
+                    .frame(width: UIScreen.main.bounds.width - 60)
+                    
+                    NavigationLink(destination: Text("login")) {
+         
+      
+                        RoundedRectangle(cornerRadius: 15)
+                          .fill(Color.white)
+                            .frame(width: UIScreen.main.bounds.width - 60, height: 50)
+                            .overlay(
+                                Text("Log In")
+                                    .foregroundColor(.black)
+                                    .padding(.vertical)
+                                    .font(.system(size: 17, weight: .semibold, design: .default))
+                                  )
+      
+                    }
+                    .padding(.vertical)
+                    
+                    NavigationLink(destination: Text("sign up")) {
+         
+      
+                        RoundedRectangle(cornerRadius: 15)
+                          .stroke(Color.white, lineWidth: 2)
+                            .frame(width: UIScreen.main.bounds.width - 60, height: 50)
+                            .overlay(
+                                Text("Sign Up")
+                                    .foregroundColor(.white)
+                                    .padding(.vertical)
+                                    .font(.system(size: 17, weight: .semibold, design: .default))
+                                  )
+                     
+                           
+                           
+                    }
+                    
+                    
+                    
+                    
+                    
+                }
+                
+                
+                
+            }
+            .navigationBarHidden(true)
         }
+            
     
        
     }
+    
+    
+    // Wave animation
+    func getSinWave(interval:CGFloat, amplitude: CGFloat = 100 ,baseline:CGFloat = UIScreen.main.bounds.height/2) -> Path {
+        Path{path in
+            path.move(to: CGPoint(x: 0, y: baseline
+            ))
+            path.addCurve(
+                to: CGPoint(x: 1*interval,y: baseline),
+                control1: CGPoint(x: interval * (0.35),y: amplitude + baseline),
+                control2: CGPoint(x: interval * (0.65),y: -amplitude + baseline)
+            )
+            path.addCurve(
+                to: CGPoint(x: 2*interval,y: baseline),
+                control1: CGPoint(x: interval * (1.35),y: amplitude + baseline),
+                control2: CGPoint(x: interval * (1.65),y: -amplitude + baseline)
+            )
+            path.addLine(to: CGPoint(x: 2*interval, y: universalSize.height))
+            path.addLine(to: CGPoint(x: 0, y: universalSize.height))
+            
+            
+        }
+    
+    }
+
 }
 
 struct LoginHomeView_Previews: PreviewProvider {
